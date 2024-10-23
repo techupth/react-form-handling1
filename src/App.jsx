578606408305be396f98bf526213e3ded03e51c1
Amornthep <amornthep.tee@gmail.com>
2024-10-23 13:16:51 +0700

    assignment-react-form-handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [greedingInput, setGreedingInput] = useState("");
+  const [greedingText, setGreedingText] = useState("Greeting Message");
+
+  const updatGreeding = () => {
+    setGreedingInput(event.target.value);
+  };
+
+  const handClick = () => {
+    setGreedingText(greedingInput);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greedingText}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={greedingInput}
+          onChange={updatGreeding}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handClick}>Update text</button>
       </div>
     </div>
   );
