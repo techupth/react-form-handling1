735cc5df65376a3abcb0e12ef877825c07ab82b1
Tonkla <Apisit378819@gmail.com>
2024-10-23 14:46:49 +0700

    feat:add jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingInputText, setGreetingInputText] = useState("");
+  const [greetingText, setGreetingText] = useState("Greeting Message");
+
+  const updateGreetingInputText = (event) => {
+    setGreetingInputText(event.target.value); // ใช้ event.target.value แทน event.target, value
+  };
+
+  const updateGreetingText = () => {
+    setGreetingText(greetingInputText);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingText}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={greetingInputText} // ใส่ค่าจาก state
+          onChange={updateGreetingInputText} // เพิ่ม onChange handler
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={updateGreetingText}>Update text</button>
       </div>
     </div>
   );
